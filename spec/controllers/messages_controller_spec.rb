a0bdc0a61aee0404c9ce598ea5c0683c0e74c5a7
kuroron985 <shigotoyou.taka@gmil.com>
2020-01-09 16:46:31 +0900

    [add/create/test] ログインしているかつ、保存に失敗した場合 まず、メッセージの保存は行われなかったかどうか

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -63,6 +63,14 @@ describe MessagesController do
     context 'can not save' do
       let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
 
+      subject {
+        post :create,
+        params: invalid_params
+        }
+
+      it 'does not count up' do
+        expect{ subject }.not_to change(Message, :count)
+      end
     end
 
     context 'not log in' do
