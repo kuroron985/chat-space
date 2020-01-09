2d215ce9bbce3d7127ddbd9347f1830a3b56e230
kuroron985 <shigotoyou.taka@gmil.com>
2020-01-09 15:10:41 +0900

    [add/test] メッセージと画像があれば保存できる

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Message, type: :model do
       it 'is valid with image' do
         expect(build(:message, content: nil)).to be_valid
       end
+
+      it 'is valid with content and image' do
+        expect(build(:message)).to be_valid
+      end
     end
 
     context 'can not save' do
