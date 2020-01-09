79162182155233c502e5d1d9f975180eb2fbbf13
kuroron985 <shigotoyou.taka@gmil.com>
2020-01-09 15:12:30 +0900

    [add/test] user_idが無いと保存できない

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -28,5 +28,12 @@ RSpec.describe Message, type: :model do
         message.valid?
         expect(message.errors[:group]).to include("を入力してください")
       end
+
+      it 'is invaid without user_id' do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include("を入力してください")
+      end
     end
   end
+end
