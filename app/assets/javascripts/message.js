8e3afc11803541c03dc852b932a05385a24d1dba
kuroron985 <shigotoyou.taka@gmil.com>
2020-01-11 08:52:35 +0900

    [add] メッセージ送信後、メッセージ画面をスクロールさせる

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -55,6 +55,7 @@ $(function () {
     .done(function (data) {
       var html = buildHTML(data);
       $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
       $('form')[0].reset();
     })
   })
