399630e591cdf40a25aaecfdead83902be220d52
kuroron985 <shigotoyou.taka@gmil.com>
2020-01-11 15:58:39 +0900

    [add] 非同期通信でユーザー検索

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,15 @@
 $(function () {
   $("#user-search-field").on("keyup", function () {
     let input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function (users) {
+      })
+      .fail(function () {
+      });
   });
 });
