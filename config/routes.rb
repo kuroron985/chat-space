c0be21465cf8de568ac15567468394e49c69cfbd
kuroron985 <shigotoyou.taka@gmil.com>
2020-01-11 20:03:38 +0900

    [add] namespaceを利用したコントローラーのルーティング設定
    1. messagesコントローラーのindexアクションをルーティング
    2. defaultsオプションを利用して、このルーティングが来たらjson形式でレスポンスするよう指定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ root to: "groups#index"
 resources :users, only: [:index,:edit, :update]
 resources :groups, only: [:new, :create, :edit, :update] do
 resources :messages, only: [:index, :create]
+
+      namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
