516740bf637b7c06ee892f1efab257c082f36fe6
kuroron985 <shigotoyou.taka@gmil.com>
2020-05-16 21:51:58 +0900

    マイグレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -28,17 +28,6 @@ ActiveRecord::Schema.define(version: 20191221022102) do
     t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "content"
-    t.string   "image"
-    t.integer  "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
@@ -55,6 +44,4 @@ ActiveRecord::Schema.define(version: 20191221022102) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
-  add_foreign_key "messages", "groups"
-  add_foreign_key "messages", "users"
 end
